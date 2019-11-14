75ecd4a5e7eef0bcdb22baea77e98c5460e8c41f
Arjun Singh <sarjun99718@gmail.com>
2019-11-14 10:40:00 +0530

    Add solution for problem `C - Dominated Subarray` Link: https://codeforces.com/contest/1257/problem/C Tags: greedy, implementation, sortings, strings, two pointers, *1200 Ref: https://codeforces.com/contest/1257/submission/64864204

--- codeforces/1257/C.py
+++ codeforces/1257/C.py
@@ -2,16 +2,24 @@ for _ in range(int(input())):
     n = int(input())
     arr = list(map(int, input().split()))
     mem = {}
-    ans = 10**7
+
     for i in range(n):
         try:
-            ans = min(i-mem[arr[i]]+1,ans)
-            mem[arr[i]] = i
+            mem[arr[i]].append(i)
         except:
-            mem[arr[i]] = i
+            mem[arr[i]] = [i]
+
+    sub = list(mem.values())
+    ans = 10**7
+    for i in sub:
+        l = len(i)
+        if l>1:
+            for j in range(1,l):
+                if (i[j]-i[j-1])<ans:
+                    ans = (i[j]-i[j-1])
     if ans==10**7:
         print(-1)
     else:
-        print(ans)
+        print(ans+1)
             
     
