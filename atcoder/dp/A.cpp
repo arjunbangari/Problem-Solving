855cf068c25ce8a143ccc70b4aa297a0673caf2c
Arjun Singh <sarjun99718@gmail.com>
2020-02-06 16:00:00 +0530

    Add solution for problem `A - Frog 1` Link: https://atcoder.jp/contests/dp/tasks/dp_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/9925994

--- atcoder/dp/A.cpp
+++ atcoder/dp/A.cpp
@@ -8,42 +8,17 @@ int main(){
     fast;
     ll n;
     cin>>n;
-    ll arr[n+1];
-    for(ll i=1;i<=n;i++)
-        cin>>arr[i];
-        
-    vector<pair<ll,ll>> v[n+1];
-    ll vis[n+1] = {0};
-    ll dist[n+1];
-    for(ll i=1;i<=n;i++)
-        dist[i] = 1e12;
-    dist[1] = 0;
+    ll h[n];
+    for(ll i=0;i<n;i++)
+        cin>>h[i];
+    ll ans[n];
+    ans[0] = 0;
+    ans[1] = abs(h[1]-h[0]);
     
-    multiset<pair<ll,ll>> ms;
+    for(ll i=2;i<n;i++)
+        ans[i] = min(ans[i-1] + abs(h[i]-h[i-1]), ans[i-2] + abs(h[i]-h[i-2]));
     
-    for(ll i=1;i<n-1;i++){
-        v[i].push_back({i+1,abs(arr[i+1]-arr[i])});
-        v[i].push_back({i+2,abs(arr[i+2]-arr[i])});
-    }
-    v[n-1].push_back({n,abs(arr[n]-arr[n-1])});
+    cout<<ans[n-1]<<endl;
     
-    ms.insert({0,1});
-    
-    while(!ms.empty()){
-        pair<ll,ll> p = *ms.begin();
-        ms.erase(ms.begin());
-        if(vis[p.second])
-            continue;
-        vis[p.second] = true;
-        
-        for(auto u: v[p.second]){
-            if(dist[p.second]+u.second<dist[u.first]){
-                dist[u.first] = dist[p.second] + u.second;
-                ms.insert({dist[u.first],u.first});
-            }
-        }
-    }
-    
-    cout<<dist[n]<<endl;
     return 0;
 }
