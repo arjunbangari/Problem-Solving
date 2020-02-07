76f959b2526380c1ce8d7a7afac865c93e40a09d
Arjun Singh <sarjun99718@gmail.com>
2020-02-07 12:30:00 +0530

    Add solution for problem `E - Tetrahedron` Link: https://codeforces.com/contest/166/problem/E Tags: dp, math, matrices, *1500 Ref: https://codeforces.com/contest/166/submission/70484618

---
+++ codeforces/166/E.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+ll m = 1000000007 ; 
+
+int main(){
+    fast;
+    ll n;
+    cin>>n;
+    ll dp[n+1],p[n+1];
+    dp[1] = 0;
+    p[1] = 1;
+    
+    for(ll i=2;i<=n;i++){
+        p[i] = (p[i-1]*3)%m;
+        dp[i] =  (p[i] - dp[i-1] + m)%m;
+    }
+    
+    cout<<dp[n]<<endl;
+    return 0;
+}
