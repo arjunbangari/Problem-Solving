dc466de16918948dbb47198721ba02f0adb21a9c
Arjun Singh <sarjun99718@gmail.com>
2020-03-04 06:15:00 +0530

    Add solution for problem `B - Kuroni and Simple Strings` Link: https://codeforces.com/contest/1305/problem/B Tags: constructive algorithms, greedy, strings, two pointers, *1200 Ref: https://codeforces.com/contest/1305/submission/72379929

--- codeforces/1305/B.cpp
+++ codeforces/1305/B.cpp
@@ -3,41 +3,59 @@ using namespace std;
 #define ll long long int
 #define endl "\n"
 #define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-
+ 
 int main(){
     fast;
     string s;
     cin>>s;
     ll sz = s.length();
-    vector<ll> ans;
+    ll pre[sz] = {0};
+    ll suf[sz] = {0};
     
-    ll l=0,r=sz-1;
+    pre[0] = (s[0]=='(');
     
-    while(l<sz && l<r){
-        if(s[l]=='('){
-            while(r>l){
-                if(s[r]==')')
-                    break;
-                r--;
-            }
-            if(r>l){
-                ans.push_back(l+1);
-                ans.push_back(r+1);
-            }
-            r--;
-        } 
-        l++;
+    for(ll i=1;i<sz;i++)
+        pre[i] = pre[i-1] + (s[i]=='(');
+    
+    suf[sz-1] = (s[sz-1]==')');
+    for(ll i=sz-2;i>=0;i--)
+        suf[i] = suf[i+1] + (s[i]==')');
+    
+    ll pos=-1;
+    for(ll i=0;i<sz;i++){
+        if(pre[i]>=suf[i]){
+            pos = i;
+            break;
+        }
     }
     
-    ll p = ans.size();
-    if(p==0){
+    if(suf[pos]==0 || pos==-1)
         cout<<0<<endl;
-    } else {
+    else{
         cout<<1<<endl;
-        cout<<p<<endl;
-        sort(ans.begin(),ans.end());
-        for(ll i=0;i<p;i++)
-            cout<<ans[i]<<" ";
+        vector<ll> ans;
+        for(ll i=0;i<=pos;i++)
+            if(s[i]=='(')
+                ans.push_back(i+1);
+        for(ll i=pos;i<sz;i++)
+            if(s[i]==')')
+                ans.push_back(i+1);
+        
+        ll temp = (pre[pos]==suf[pos]);
+        
+        ll sa = ans.size();
+        
+        if(temp){
+            cout<<sa<<endl;
+            for(ll i=0;i<sa;i++)
+                cout<<ans[i]<<" ";
+        } else {
+            cout<<sa-1<<endl;
+            for(ll i=0;i<sa/2;i++)
+                cout<<ans[i]<<" ";
+            for(ll i=sa/2+1; i<sa;i++)
+                cout<<ans[i]<<" ";
+        }
     }
     
     return 0;
