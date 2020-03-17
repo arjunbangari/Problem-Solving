083d913cd60ca2ad8c53b4fc39e3bcb4b27efd34
Arjun Singh <sarjun99718@gmail.com>
2020-03-17 13:11:00 +0530

    Add solution for problem `A - Oh Those Palindromes` Link: https://codeforces.com/contest/1063/problem/A Tags: constructive algorithms, strings, *1300 Ref: https://codeforces.com/contest/1063/submission/73462756

---
+++ codeforces/1063/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+int main(){
+    fast;
+    ll n;
+    cin>>n;
+    string s;
+    cin>>s;
+    
+    sort(s.begin(), s.end());
+    
+    cout<<s<<endl;
+    
+    return 0;
+}
