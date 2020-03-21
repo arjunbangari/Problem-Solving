82eef73bfb8355c17c44dad409a7ed869b970b7a
Arjun Singh <sarjun99718@gmail.com>
2020-03-21 17:05:00 +0530

    Add solution for problem `D1 - Magic Powder - 1` Link: https://codeforces.com/contest/670/problem/D1 Tags: binary search, brute force, implementation, *1400 Ref: https://codeforces.com/contest/670/submission/73881944

---
+++ codeforces/670/D1.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef unsigned long long ll;
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+int main(){
+    fast;
+    ll n,k;
+    cin>>n>>k;
+    
+    ll arr[n],brr[n];
+    for(ll i=0;i<n;i++)
+        cin>>arr[i];
+        
+    for(ll i=0;i<n;i++)
+        cin>>brr[i];
+    
+    ll l=0, r = 2e9;
+    
+    while(l<r){
+        ll mid = (l+r+1)/2;
+        ll extra = 0;
+        
+        for(ll i=0;i<n;i++){
+            ll required = arr[i]*mid;
+            if(required>brr[i])
+                extra += (required-brr[i]);
+        }
+        
+        if(extra<=k)
+            l = mid;
+        else
+            r = mid-1;
+    }
+    
+    cout<<l<<endl;
+    
+    return 0;
+}
