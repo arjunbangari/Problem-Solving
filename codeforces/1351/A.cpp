ff8c2641452030febca629cb79f8343388b9f1be
Arjun Singh <sarjun99718@gmail.com>
2020-05-07 19:35:00 +0530

    Add solution for problem `A - A+B (Trial Problem)` Link: https://codeforces.com/contest/1351/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1351/submission/79277386

---
+++ codeforces/1351/A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef long double ld;
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+ll const inf = 1e18;
+ll const maxn = 1e6+5;
+ll const mod = 1e9+7;
+
+// code begins here
+
+int main(){
+    fast;
+    
+    ll t;
+    cin>>t;
+    
+    while(t--){
+    ll a,b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+    }
+    
+    return 0;
+}
