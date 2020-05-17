e8d7a18e106854fee742c6b68a25ca756cc4f331
Arjun Singh <sarjun99718@gmail.com>
2020-05-17 11:46:00 +0530

    Add solution for problem `D2 - Prefix-Suffix Palindrome (Hard version)` Link: https://codeforces.com/contest/1326/problem/D2 Tags: binary search, greedy, hashing, string suffix structures, strings, *1800 Ref: https://codeforces.com/contest/1326/submission/80438293

--- codeforces/1326/D2.cpp
+++ codeforces/1326/D2.cpp
@@ -1,83 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 typedef long long int ll;
-typedef long double ld;
-typedef pair<ll,ll> pll;
-#define endl "\n";
+#define endl "\n"
 #define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 
-ll const inf = 1e18;
-ll const maxn = 1e6+5;
-ll const mod = 1e9+7;
+//check
 
-// code begins here
-
-string secret = "skdsksdsddssdsd";
-vector<ll> pre(maxn);
-string a,b;
-ll a_hash = 0 , b_hash = 0, n1, inverse, key=26;
-
-void compute_key(){
-    key = 0;
-    for(ll i=1;i<secret.length();i++)
-        key = ( key + pre[i]*(secret[i]-'a'+1))%mod;
-}
-
-void precompute(){
-    pre[0] = 1;
-    for(ll i=1;i<maxn;i++)
-        pre[i] = (pre[i-1]*key)%mod;
-}
-
-ll modInverse(ll a, ll m) { 
-    ll m0 = m; 
-    ll y = 0, x = 1; 
-    if (m == 1) 
-      return 0; 
-  
-    while (a > 1) { 
-        ll q = a / m; 
-        ll t = m; 
-        m = a % m, a = t; 
-        t = y; 
-        y = x - q * y; 
-        x = t; 
-    } 
-    if (x < 0) 
-       x += m0; 
-    return x; 
-} 
-
-void compute_hash(){
-    for(ll i=0;i<n1;i++){
-        a_hash = (a_hash + pre[i]*(a[i]-'a'+1))%mod;
-        b_hash = (b_hash + pre[i]*(b[i]-'a'+1))%mod;
-    }
-    
-}
-
-string longest_prefix_palindrome(){
-    b = a;
-    reverse(b.begin(), b.end());
-    
-    compute_hash();
-    
-    ll i = n1-1;
-    
-    while(i>=0){
-        if(a_hash==b_hash)
-            break;
-        
-        a_hash = (a_hash - ((a[i]-'a'+1)*pre[i])%mod + mod)%mod;
-        b_hash = (b_hash - (a[i]-'a'+1) + mod)%mod;
-        b_hash = ( b_hash * inverse)%mod;
-        i--;
-    }
-    
-    return a.substr(0,i+1);
-    
-}
+ll fail1 [2000005];
+ll fail2 [2000005];
 
 int main(){
     fast;
@@ -85,61 +15,52 @@ int main(){
     ll t;
     cin>>t;
     
-    precompute();
-    compute_key();
-    precompute();
-    
-    inverse = modInverse(key, mod);
-    inverse %= mod;
-    
     while(t--){
-        string s;
-        cin>>s;
+        string str;
+        cin>>str;
+        ll n = str.length();
         
-        ll n = s.length();
-        
-        //Find palindrome from both sides
-        
-        ll i = 0;
-        while(i<n && s[i]==s[n-i-1])
+        ll i=0, j=n-1;
+        while(i<j && str[i]==str[j]){
             i++;
-        
-        if(i==n){
-            cout<<s<<endl;
-            continue;
+            j--;
         }
         
-        string ans = s.substr(0,i);
-        string ans2 = ans;
-        reverse(ans2.begin(), ans2.end());
-        
-        // Remaining string
-        a = s.substr(i,n - 2*i);
-        
-        // length of remaining string
-        n1 = n - 2*i;
-        
-        // calculate longest_prefix_palindrome for string
-        a_hash = 0; b_hash = 0;
-        string fi = longest_prefix_palindrome();
-        
-        // cout<<fi<<endl;
-        
-        //calculate longest_prefix_palindrome for reverse
-        a = b;
-        a_hash = 0; b_hash = 0;
-        string se = longest_prefix_palindrome();
-        reverse(se.begin(), se.end());
-        
-        ll c = fi.length(), d = se.length();
-        
-        if(c>d){
-            ans += fi + ans2;
+        if(i>=j){
+            cout<<str<<endl;
         } else {
-            ans += se + ans2;
+            ll str_len = n - 2*i ;
+            
+            string s, c, u, v , prefix, suffix;
+            
+            u = str.substr(0,i);
+            v = str.substr(n-i,i);
+            s = str.substr(i,str_len);
+            c = s;
+            reverse(c.begin(), c.end());
+            prefix = s + "#" + c;
+            suffix = c + "#" + s;
+            
+            for(ll i = 1, k = 0; i < prefix.size(); i++){
+                while(k > 0 && prefix[i] != prefix[k]) k = fail1[k - 1];
+                if(prefix[i] == prefix[k]) k++;
+                fail1[i] = k;
+            }
+            for(ll i = 1, k = 0; i < suffix.size(); i++){
+                while(k > 0 && suffix[i] != suffix[k]) k = fail2[k - 1];
+                if(suffix[i] == suffix[k]) k++;
+                fail2[i] = k;
+            }
+            string res1 = s.substr(s.size() - fail2[suffix.size() - 1], fail2[suffix.size() - 1]);
+            string res2 = s.substr(0 , fail1[prefix.size() - 1]);
+            
+            ll r1 = res1.length(), r2 = res2.length();
+            
+            if(r1>r2)
+                cout<<u + res1 + v <<endl;
+            else
+                cout<<u + res2 + v <<endl;
         }
-        
-        cout<<ans<<endl;
         
     }
     
