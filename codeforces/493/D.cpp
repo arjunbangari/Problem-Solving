af8b11e03ca31ba7ddfc0017355b4493ccc3ced3
Arjun Singh <sarjun99718@gmail.com>
2020-06-19 16:10:00 +0530

    Add solution for problem `D - Vasya and Chess` Link: https://codeforces.com/contest/493/problem/D Tags: constructive algorithms, games, math, *1700 Ref: https://codeforces.com/contest/493/submission/84316343

---
+++ codeforces/493/D.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef long double ld;
+typedef pair<ll,ll> pll;
+#define endl "\n";
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+ll const inf = 1e18;
+ll const maxn = 2e5+5;
+ll const mod = 1e9+7;
+
+// code begins here
+
+int main(){
+    fast;
+    
+    ll n;
+    cin>>n;
+    
+    if(n%2){
+        cout<<"black"<<endl;
+    } else {
+        cout<<"white"<<endl;
+        cout<<"1 2"<<endl;
+    }
+    
+    return 0;
+}
