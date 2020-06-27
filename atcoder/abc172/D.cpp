3c6701b80c3981270341190d808a1fd32a664533
Arjun Singh <sarjun99718@gmail.com>
2020-06-27 17:46:00 +0530

    Add solution for problem `D - Sum of Divisors` Link: https://atcoder.jp/contests/abc172/tasks/abc172_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc172/submissions/14742426

--- atcoder/abc172/D.cpp
+++ atcoder/abc172/D.cpp
@@ -1,29 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 typedef long long int ll;
 typedef long double ld;
-#define endl "\n"
+typedef pair<ll,ll> pll;
+#define endl "\n";
 #define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-
+ 
 ll const inf = 1e18;
 ll const maxn = 1e6+5;
-ll const mod = 1e9+7;
-
+ll const mod = 998244353;
+ 
 // code begins here
 
 int main(){
     fast;
     
-    ll n, ans=0;
+    ll n;
     cin>>n;
     
+    vector<ll> cnt(n+1);
+    
     for(ll i=1;i<=n;i++){
-        ll x = n/i;
-        ans += i*(x*(x+1)/2);
+        for(ll j=i;j<=n;j+=i){
+            cnt[j]++;
+        }
     }
     
+    ll ans = 0;
+    
+    for(ll i=1;i<=n;i++)
+        ans += i*(cnt[i]);
+    
     cout<<ans<<endl;
+        
     
     return 0;
 }
