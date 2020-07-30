1fc27a6bc6f04e9be4f8414460266f45088d7237
Arjun Singh <sarjun99718@gmail.com>
2020-07-30 20:19:00 +0530

    Add solution for problem `B - Captain Flint and a Long Voyage` Link: https://codeforces.com/contest/1388/problem/B Tags: greedy, math, *1000 Ref: https://codeforces.com/contest/1388/submission/88463206

--- codeforces/1388/B.cpp
+++ codeforces/1388/B.cpp
@@ -1,28 +1,49 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long int lli;
-typedef double db;
+
+typedef long long int ll;
+typedef long double ld;
+typedef pair<ll,ll> pll;
+#define all(v) (v).begin(),(v).end()
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+ll const inf = 1e18;
+ll const maxn = 1e6+5;
+ll const mod = 1e9+7;
+
+// code begins here
+
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	lli t;
-	cin>>t;
-	while(t--){
-		lli n;
-		cin>>n;
-		string s="";
-		for(lli i=0;i<n;i++){
-			s += '9';
-		}
-		lli tm=n/4;
-		lli tp=n%4;
-		for(lli i=0;i<tm;i++){
-			s[n-i-1]='8';
-		}
-		if(tp>=1){
-			s[n-tm-1]='8';
-		}
-		cout<<s<<"\n";
-	}
-	return 0;
+    fast;
+    
+    ll t;
+    cin>>t;
+    
+    while(t--){
+        ll n;
+        cin>>n;
+        
+        vector<ll> ans(n);
+        for(ll i=0;i<n;i++)
+            ans[i] = 9;
+        
+        if(n%4==0){
+            ll x = n/4;
+            for(ll i=n-1;i>n-1-x;i--){
+                ans[i] = 8;
+            }
+        } else {
+                ll x = n/4;
+                for(ll i=n-1;i>=n-1-x;i--){
+                    ans[i] = 8;
+                 }
+        }
+        
+        for(ll i=0;i<n;i++)
+            cout<<ans[i];
+        cout<<endl;
+    }
+    
+    return 0;
 }
