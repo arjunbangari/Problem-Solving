cf24449c916fcd5b58a6b41e5922d3b743f79c30
Arjun Singh <sarjun99718@gmail.com>
2020-08-24 21:47:00 +0530

    Add solution for problem `K - Stones` Link: https://atcoder.jp/contests/dp/tasks/dp_k Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/16209888

--- atcoder/dp/K.cpp
+++ atcoder/dp/K.cpp
@@ -15,54 +15,26 @@ ll const mod = 1e9+7;
 
 // code begins here
 
-vector<ll> arr;
-ll dp[maxn][2];
-
-void recur( ll k, ll cur ){
-    if(dp[k][cur]!=-1)
-        return;
-    
-    if(k==0){
-        dp[k][cur] = !cur;
-        return;
-    }
-    
-    ll ans = 0;
-    for(auto u: arr){
-        if( k >= u ){
-            recur(k-u, !cur);
-        }
-    }
-    
-    dp[k][cur] = !cur;
-    
-    for(auto u: arr){
-        if( k>=u && dp[k-u][!cur]==cur){
-            dp[k][cur] = cur;
-        }
-    }
-}
-
 int main(){
     fast;
     
-    ll n,k;
+    ll n, k;
     cin>>n>>k;
     
-    for(ll i=0;i<n;i++){
-        ll x;
-        cin>>x;
-        arr.push_back(x);
+    vector<ll> arr(n);
+    for(auto &u: arr)
+        cin>>u;
+    
+    vector<ll> dp(k+1);
+    
+    for(ll i=0;i<=k;i++){
+        for(auto u: arr){
+            if( i>=u && !dp[i-u] )
+                dp[i] = 1;
+        }
     }
     
-    for(ll i=0;i<maxn;i++){
-        dp[i][0] = -1;
-        dp[i][1] = -1;
-    }
-    
-    recur(k, 1);
-    
-    cout<< ( dp[k][1] ? "First" : "Second" ) <<endl;
+    cout<< ( dp[k] ? "First" : "Second" ) <<endl;
     
     return 0;
 }
