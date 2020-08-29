bfa5a1b63e336415256ab57533c573672e9ec064
Arjun Singh <sarjun99718@gmail.com>
2020-08-29 17:31:00 +0530

    Add solution for problem `A - Don't be late` Link: https://atcoder.jp/contests/abc177/tasks/abc177_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc177/submissions/16306285

---
+++ atcoder/abc177/A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef long double ld;
+typedef pair<ll,ll> pll;
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+#define endl "\n"
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+ll const inf = 1e18;
+ll const maxn = 1e6+5;
+ll const mod = 1e9+7;
+
+// code begins here
+
+int main(){
+    fast;
+    
+    ll d,t,s;
+    cin>>d>>t>>s;
+    
+    cout<< ( s*t>=d ? "Yes" : "No") <<endl;
+    
+    return 0;
+}
